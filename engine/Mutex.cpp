260fc92b9d823a1e90978a4b0d74457a73ced324
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2004-10-14 17:27:03 +0000

    Added global mutex to protect other mutexes' data structures.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@82 acf43c95-373e-0410-b603-e72c3f656dc1

--- engine/Mutex.cpp
+++ engine/Mutex.cpp
@@ -28,26 +28,62 @@ private:
     volatile bool m_locked;
 };
 
+class GlobalMutex {
+public:
+    GlobalMutex();
+    inline static void init()
+	{ if (s_init) { s_init = false; ::pthread_mutex_init(&s_mutex,0); } }
+    static void lock();
+    static void unlock();
+private:
+    static bool s_init;
+    static pthread_mutex_t s_mutex;
+};
+
 };
 
 using namespace TelEngine;
 
+GlobalMutex s_global;
+
 volatile int MutexPrivate::s_count = 0;
 volatile int MutexPrivate::s_locks = 0;
+bool GlobalMutex::s_init = true;
+pthread_mutex_t GlobalMutex::s_mutex;
 
 // WARNING!!!
 // No debug messages are allowed in mutexes since the debug output itself
 // is serialized using a mutex!
 
+GlobalMutex::GlobalMutex()
+{
+    init();
+}
+
+void GlobalMutex::lock()
+{
+    init();
+    ::pthread_mutex_lock(&s_mutex);
+}
+
+void GlobalMutex::unlock()
+{
+    init();
+    ::pthread_mutex_unlock(&s_mutex);
+}
+
 MutexPrivate::MutexPrivate()
     : m_refcount(1), m_locked(false)
 {
+    GlobalMutex::lock();
     s_count++;
     ::pthread_mutex_init(&m_mutex,0);
+    GlobalMutex::unlock();
 }
 
 MutexPrivate::~MutexPrivate()
 {
+    GlobalMutex::lock();
     if (m_locked) {
 	m_locked = false;
 	s_locks--;
@@ -55,6 +91,7 @@ MutexPrivate::~MutexPrivate()
     }
     s_count--;
     ::pthread_mutex_destroy(&m_mutex);
+    GlobalMutex::unlock();
 }
 
 bool MutexPrivate::lock(long long int maxwait)
@@ -74,27 +111,36 @@ bool MutexPrivate::lock(long long int maxwait)
 	    ::usleep(1);
 	} while (t > Time::now());
     }
+    GlobalMutex::lock();
     if (rval) {
 	s_locks++;
 	m_locked = true;
     }
     else
 	deref();
+    GlobalMutex::unlock();
     return rval;
 }
 
 void MutexPrivate::unlock()
 {
     // Hope we don't hit a bug related to the debug mutex!
+    GlobalMutex::lock();
     if (m_locked) {
 	m_locked = false;
-	if (--s_locks < 0)
+	if (--s_locks < 0) {
+	    GlobalMutex::unlock();
 	    Debug(DebugFail,"MutexPrivate::locks() is %d",s_locks);
+	    GlobalMutex::lock();
+	}
 	::pthread_mutex_unlock(&m_mutex);
+	GlobalMutex::unlock();
 	deref();
     }
-    else
+    else {
+	GlobalMutex::unlock();
 	Debug(DebugFail,"MutexPrivate::unlock called on unlocked mutex");
+    }
 }
 
 Mutex::Mutex()

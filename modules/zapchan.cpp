15649df4dfebd328d91ba9c2b89d95224ba42b49
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2004-10-30 16:52:03 +0000

    Only log hangups if channel was not idle.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@99 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/zapchan.cpp
+++ modules/zapchan.cpp
@@ -1057,7 +1057,8 @@ bool ZapChan::answer()
 
 void ZapChan::hangup(int cause)
 {
-    Debug("ZapChan",DebugAll,"Hanging up channel %d in state %s",m_abschan,status());
+    if (inUse())
+	Debug(DebugInfo,"Hanging up zap/%d in state %s",m_abschan,status());
     m_ring = false;
     m_timeout = 0;
     if (m_call) {

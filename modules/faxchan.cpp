656e42347458d28ccb64f7993171b8f138934e36
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2004-11-29 03:56:41 +0000

    Added copyright notice.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@114 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/faxchan.cpp
+++ modules/faxchan.cpp
@@ -8,11 +8,27 @@
  * This great software can be found at 
  * ftp://opencall.org/pub/spandsp/
  * 
- * Fax driver (trasmision+receiving)
+ * Fax driver (transmission+receiving)
+ *
+ * Yet Another Telephony Engine - a fully featured software PBX and IVR
+ * Copyright (C) 2004 Null Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-// Ask for various C99 shit. The library author is a sicko (paulc)
-// Ja right man, exacly, in this case you should rewrite his entire work, if you don't like it  (diana)
+// For SpanDSP we have to ask for various C99 stuff
 #define __USE_ISOC99
 #define __STDC_LIMIT_MACROS
 

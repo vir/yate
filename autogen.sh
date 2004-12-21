7ac9803be5763da36bfee7771867d0b66d897ec7
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2004-12-21 04:31:37 +0000

    Added autogen script so it is easier to bootstrap.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@134 acf43c95-373e-0410-b603-e72c3f656dc1

---
+++ autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Run this to generate a new configure script
+
+if [ -s tables/a2s.h ]; then
+    echo "Good! Tables are generated so we don't need sox."
+else
+    if [ -z `which sox &>/dev/null` ]; then
+	echo "Please install sox to be able to build from CVS version." >&2
+	exit 1
+    fi
+fi
+
+autoconf

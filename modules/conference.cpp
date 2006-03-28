306896ba307105419f0737e60dd14b1a7363a5f1
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2006-03-28 23:19:35 +0000

    Gave credits to Andrew McDonald for N-way no-echo mixing.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@727 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/conference.cpp
+++ modules/conference.cpp
@@ -7,6 +7,8 @@
  * Yet Another Telephony Engine - a fully featured software PBX and IVR
  * Copyright (C) 2004, 2005 Null Team
  *
+ * N-way mixing with self echo suppresion idea by Andrew McDonald
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or

7861c1499bc1a70066e6da1145654ee368b2d537
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2006-05-27 13:49:50 +0000

    Updated copyright updating scripts, added one for FSF address.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@812 acf43c95-373e-0410-b603-e72c3f656dc1

--- tools/upcopy.sh
+++ tools/upcopy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 cpy="$1"
-test -n "$cpy" && cpy="2004, 2005"
+test -n "$cpy" || cpy="2004-2006"
 
 grep -l -r '^ \* Copyright (C) .* Null Team$' * | while read fn; do
-echo $fn
+echo -n "Processing: $fn ..."
 sed 's/^\( \* Copyright (C) \).*\( Null Team\)$/\1'"$cpy"'\2/' < "$fn" > "$fn.tmp"
 if cmp -s "$fn" "$fn.tmp"; then
-    echo "Unchanged: $fn"
+    echo " unchanged"
     rm "$fn.tmp"
 else
-    echo "Changed: $fn"
+    echo " changed"
     mv "$fn.tmp" "$fn"
 fi
 done

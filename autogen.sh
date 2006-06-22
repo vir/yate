2a5a67068791a9225afbd1b8a2d2cc2a0ccde7b6
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2006-06-22 18:32:30 +0000

    Try to guess autoconf's location in freebsd.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@899 acf43c95-373e-0410-b603-e72c3f656dc1

--- autogen.sh
+++ autogen.sh
@@ -11,4 +11,11 @@ else
     fi
 fi
 
-autoconf && echo "Finished! Now run configure. If in doubt run ./configure --help"
+ac=`which autoconf 2>/dev/null`
+test -z "$ac" && ac=/usr/local/gnu-autotools/bin/autoconf
+if [ -x "$ac" ]; then
+    "$ac" && echo "Finished! Now run configure. If in doubt run ./configure --help"
+else
+    echo "Please install Gnu autoconf to build from CVS." >&2
+    exit 1
+fi

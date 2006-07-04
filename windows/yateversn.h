25fdfea3fbc84e857c97428f98264770fc92ca01
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2006-07-04 19:11:58 +0000

    Bumped version no, preparing for release.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@929 acf43c95-373e-0410-b603-e72c3f656dc1

--- windows/yateversn.h
+++ windows/yateversn.h
@@ -1,4 +1,4 @@
-/**
+/*
  * yateversn.h
  * This file is part of the YATE Project http://YATE.null.ro
  *
@@ -10,19 +10,19 @@
 #define __YATEVERSN_H
 
 /* Version numbers */
-#define YATE_MAJOR 0
-#define YATE_MINOR 9
+#define YATE_MAJOR 1
+#define YATE_MINOR 0
 #define YATE_BUILD 0
 
 /* Version strings */
-#define YATE_MAJOR_S "0"
-#define YATE_MINOR_S "9"
+#define YATE_MAJOR_S "1"
+#define YATE_MINOR_S "0"
 #define YATE_BUILD_S "0"
-#define YATE_VERSION "0.9.0"
-#define YATE_RELEASE "pre4"
+#define YATE_VERSION "1.0.0"
+#define YATE_RELEASE "pre1"
 
 /* Windows version resource - file and string style */
-#define YATE_WINVER_F 0,9,0,0
-#define YATE_WINVER_S "0, 9, 0\0"
+#define YATE_WINVER_F 1,0,0,0
+#define YATE_WINVER_S "1, 0, 0, 0\0"
 
 #endif /* __YATEVERSN_H */

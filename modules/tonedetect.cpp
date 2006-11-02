04a11f732fec7da441440841a9c8ef219f0a49db
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2006-11-02 23:29:43 +0000

    Added DTMF detection and CED detection as alternative to CNG.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@1101 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/tonedetect.cpp
+++ modules/tonedetect.cpp
@@ -34,6 +34,11 @@ namespace { // anonymous
 #define THRESHOLD2_ABS 5e+06
 // relative square of spectral power from total signal power
 #define THRESHOLD2_REL_FAX 0.85
+#define THRESHOLD2_REL_ALL 0.75
+#define THRESHOLD2_REL_DTMF 0.2
+
+// minimum DTMF detect time
+#define DETECT_DTMF_MSEC 32
 
 // 2-pole filter parameters
 typedef struct
@@ -47,12 +52,17 @@ typedef struct
 class Tone2PoleFilter
 {
 public:
+    inline Tone2PoleFilter()
+	: m_mult(0.0), m_y0(0.0), m_y1(0.0)
+	{ }
     inline Tone2PoleFilter(double gain, double y0, double y1)
 	: m_mult(1.0/gain), m_y0(y0), m_y1(y1)
 	{ init(); }
     inline Tone2PoleFilter(const Params2Pole& params)
 	: m_mult(1.0/params.gain), m_y0(params.y0), m_y1(params.y1)
 	{ init(); }
+    inline void assign(const Params2Pole& params)
+	{ m_mult = 1.0/params.gain; m_y0 = params.y0; m_y1 = params.y1; init(); }
     inline void init()
 	{ m_val = m_y[1] = m_y[2] = 0.0; }
     inline double value() const
@@ -68,6 +78,7 @@ private:
 
 class ToneConsumer : public DataConsumer
 {
+    YCLASS(ToneConsumer,DataConsumer)
 public:
     ToneConsumer(const String& id, const String& name);
     virtual ~ToneConsumer(); 
@@ -79,15 +90,22 @@ public:
     void setDivert(const Message& msg);
     void init();
 private:
+    void checkDtmf();
+    void checkFax();
     String m_id;
     String m_name;
     String m_divert;
     String m_caller;
     String m_called;
-    bool m_found;
+    bool m_detFax;
+    bool m_detDtmf;
+    char m_dtmfTone;
+    int m_dtmfCount;
     double m_xv[3];
     double m_pwr;
     Tone2PoleFilter m_fax;
+    Tone2PoleFilter m_dtmfL[4];
+    Tone2PoleFilter m_dtmfH[4];
 };
 
 class AttachHandler : public MessageHandler
@@ -110,9 +128,13 @@ public:
     ToneDetectorModule();
     virtual ~ToneDetectorModule();
     virtual void initialize();
+    virtual void statusParams(String& str);
 private:
     bool m_first;
 };
+
+static Mutex s_mutex;
+static int s_count = 0;
 
 static ToneDetectorModule plugin;
 
@@ -121,21 +143,33 @@ static ToneDetectorModule plugin;
 //  -> 2-pole resonator bandpass, 1100Hz, Q-factor=50
 // mkfilter -Bu -Bp -o 1 -a 1.3612500000e-01 1.3887500000e-01
 //  -> 2-pole butterworth bandpass, 1100Hz +-11Hz @ -3dB
-static Params2Pole s_paramsFax =
+static Params2Pole s_paramsCNG =
     { 1.167453752e+02, -0.9828688170, 1.2878183436 }; // 1100Hz
 
+// generated CED detector (2100Hz) filter parameters
+// mkfilter -Bu -Bp -o 1 -a 2.6062500000e-01 2.6437500000e-01
+//  -> 2-pole butterworth bandpass, 2100Hz +-15Hz @ -3dB
+static Params2Pole s_paramsCED =
+    { 8.587870006e+01, -0.9767113407, -0.1551017476 }; // 2100Hz
+
 // generated DTMF component filter parameters
-// 2-pole butterworth bandpass, +-10% @ -3dB
-static Params2Pole s_paramsDtmf[] = {
+// 2-pole butterworth bandpass, +-1% @ -3dB
+static Params2Pole s_paramsDtmfL[] = {
     { 1.836705768e+02, -0.9891110494, 1.6984655220 }, // 697Hz
     { 1.663521771e+02, -0.9879774290, 1.6354206881 }, // 770Hz
     { 1.504376844e+02, -0.9867055777, 1.5582944783 }, // 852Hz
     { 1.363034877e+02, -0.9853269818, 1.4673997821 }, // 941Hz
-
+};
+static Params2Pole s_paramsDtmfH[] = {
     { 1.063096655e+02, -0.9811871438, 1.1532059506 }, // 1209Hz
     { 9.629842594e+01, -0.9792313229, 0.9860778489 }, // 1336Hz
     { 8.720029263e+01, -0.9770643703, 0.7895131023 }, // 1477Hz
     { 7.896493565e+01, -0.9746723483, 0.5613790789 }, // 1633Hz
+};
+
+// DTMF table using low, high indexes
+static char s_tableDtmf[][5] = {
+    "123A", "456B", "789C", "*0#D"
 };
 
 // Update a moving average with square of value (so we end with ~ power)
@@ -156,17 +190,48 @@ void Tone2PoleFilter::update(double xd)
 
 
 ToneConsumer::ToneConsumer(const String& id, const String& name)
-    : m_id(id), m_name(name), m_found(false),
-      m_fax(s_paramsFax)
+    : m_id(id), m_name(name), m_detFax(true), m_detDtmf(true),
+      m_fax(s_paramsCNG)
 { 
     Debug(&plugin,DebugAll,"ToneConsumer::ToneConsumer(%s,'%s') [%p]",
 	id.c_str(),name.c_str(),this);
+    for (int i = 0; i < 4; i++) {
+	m_dtmfL[i].assign(s_paramsDtmfL[i]);
+	m_dtmfH[i].assign(s_paramsDtmfH[i]);
+    }
     init();
+    String tmp = name;
+    if (tmp.find('/') >= 0)
+	tmp >> "/";
+    if (tmp && (tmp != "*")) {
+	// individual detection requested
+	m_detFax = m_detDtmf = false;
+	ObjList* k = tmp.split(',',false);
+	for (ObjList* l = k; l; l = l->next()) {
+	    String* s = static_cast<String*>(l->get());
+	    if (!s)
+		continue;
+	    m_detFax = m_detFax || (*s == "fax");
+	    m_detDtmf = m_detDtmf || (*s == "dtmf");
+	    if (*s == "rfax") {
+		// detection of receiving Fax requested
+		m_fax.assign(s_paramsCED);
+		m_detFax = true;
+	    }
+	}
+	delete k;
+    }
+    s_mutex.lock();
+    s_count++;
+    s_mutex.unlock();
 }
 
 ToneConsumer::~ToneConsumer()
 {
     Debug(&plugin,DebugAll,"ToneConsumer::~ToneConsumer [%p]",this);
+    s_mutex.lock();
+    s_count--;
+    s_mutex.unlock();
 }
 
 // Re-init filter(s)
@@ -175,42 +240,124 @@ void ToneConsumer::init()
     m_xv[1] = m_xv[2] = 0.0;
     m_pwr = 0.0;
     m_fax.init();
+    for (int i = 0; i < 4; i++) {
+	m_dtmfL[i].init();
+	m_dtmfH[i].init();
+    }
+    m_dtmfTone = '\0';
+    m_dtmfCount = 0;
+}
+
+// Check if we detected a DTMF
+void ToneConsumer::checkDtmf()
+{
+    int i;
+    char c = m_dtmfTone;
+    m_dtmfTone = '\0';
+    int l = 0;
+    double maxL = m_dtmfL[0].value();
+    for (i = 1; i < 4; i++) {
+	if (maxL < m_dtmfL[i].value()) {
+	    maxL = m_dtmfL[i].value();
+	    l = i;
+	}
+    }
+    if (maxL < m_pwr*THRESHOLD2_REL_DTMF)
+	return;
+    int h = 0;
+    double maxH = m_dtmfH[0].value();
+    for (i = 1; i < 4; i++) {
+	if (maxH < m_dtmfH[i].value()) {
+	    maxH = m_dtmfH[i].value();
+	    h = i;
+	}
+    }
+    if (maxH < m_pwr*THRESHOLD2_REL_DTMF)
+	return;
+    if ((maxL+maxH) < m_pwr*THRESHOLD2_REL_ALL)
+	return;
+    char buf[2];
+    buf[0] = s_tableDtmf[l][h];
+    buf[1] = '\0';
+    if (buf[0] != c) {
+	XDebug(&plugin,DebugInfo,"DTMF '%s' new candidate on %s, lo=%f, hi=%f, total=%f",
+	    buf,m_id.c_str(),maxL,maxH,m_pwr);
+	m_dtmfTone = buf[0];
+	m_dtmfCount = 1;
+	return;
+    }
+    m_dtmfTone = c;
+    XDebug(&plugin,DebugInfo,"DTMF '%s' candidate %d on %s, lo=%f, hi=%f, total=%f",
+	buf,m_dtmfCount,m_id.c_str(),maxL,maxH,m_pwr);
+    if (m_dtmfCount++ == DETECT_DTMF_MSEC) {
+	DDebug(&plugin,DebugInfo,"DTMF '%s' detected on %s, lo=%f, hi=%f, total=%f",
+	    buf,m_id.c_str(),maxL,maxH,m_pwr);
+	Message *m = new Message("chan.masquerade");
+	m->addParam("id",m_id);
+	m->addParam("message","chan.dtmf");
+	m->addParam("text",buf);
+	Engine::enqueue(m);
+    }
+}
+
+// Check if we detected a Fax CNG tone
+void ToneConsumer::checkFax()
+{
+    if (m_fax.value() < m_pwr*THRESHOLD2_REL_FAX)
+	return;
+    DDebug(&plugin,DebugInfo,"Fax detected on %s, signal=%f, total=%f",
+	m_id.c_str(),m_fax.value(),m_pwr);
+    // prepare for new detection
+    init();
+    m_detFax = false;
+    Message* m = new Message("chan.masquerade");
+    m->addParam("id",m_id);
+    if (m_divert) {
+	Debug(&plugin,DebugCall,"Diverting call %s to: %s",
+	    m_id.c_str(),m_divert.c_str());
+	m->addParam("message","call.execute");
+	m->addParam("callto",m_divert);
+	m->addParam("reason","fax");
+    }
+    else
+	m->addParam("message","call.fax");
+    Engine::enqueue(m);
 }
 
 // Feed samples to the filter(s)
 void ToneConsumer::Consume(const DataBlock& data, unsigned long timeDelta)
 {
-    if (m_found || data.null())
-	return;
     const int16_t* s = (const int16_t*)data.data();
+    if (!s)
+	return;
     for (unsigned int i=0; i<data.length(); i+=2) {
 	m_xv[0] = m_xv[1]; m_xv[1] = m_xv[2];
 	m_xv[2] = *s++;
-	m_fax.update(m_xv[2] - m_xv[0]);
-
-	// is it enough total power to accept a signal?
+	double dx = m_xv[2] - m_xv[0];
 	updatePwr(m_pwr,m_xv[2]);
-	if (m_pwr < THRESHOLD2_ABS)
-	    continue;
-	if (m_fax.value() > m_pwr*THRESHOLD2_REL_FAX) {
-	    DDebug(&plugin,DebugInfo,"Fax detected on %s, signal=%f, total=%f",
-		m_id.c_str(),m_fax.value(),m_pwr);
-	    // prepare for new detection
-	    init();
-	    m_found = true;
-	    Message* m = new Message("chan.masquerade");
-	    m->addParam("id",m_id);
-	    if (m_divert) {
-		Debug(&plugin,DebugCall,"Diverting call %s to: %s",
-		    m_id.c_str(),m_divert.c_str());
-		m->addParam("message","call.execute");
-		m->addParam("callto",m_divert);
-		m->addParam("reason","fax");
+
+	// update all active detectors
+	if (m_detFax)
+	    m_fax.update(dx);
+	if (m_detDtmf) {
+	    for (int j = 0; j < 4; j++) {
+		m_dtmfL[j].update(dx);
+		m_dtmfH[j].update(dx);
 	    }
-	    else
-		m->addParam("message","call.fax");
-	    Engine::enqueue(m);
-	    return;
+	}
+	// only do checks evey millisecond
+	if (i % 8)
+	    continue;
+	// is it enough total power to accept a signal?
+	if (m_pwr >= THRESHOLD2_ABS) {
+	    if (m_detDtmf)
+		checkDtmf();
+	    if (m_detFax)
+		checkFax();
+	}
+	else {
+	    m_dtmfTone = '\0';
+	    m_dtmfCount = 0;
 	}
     }
     XDebug(&plugin,DebugInfo,"Fax detector on %s: signal=%f, total=%f",
@@ -223,6 +370,7 @@ void ToneConsumer::setDivert(const Message& msg)
     NamedString* divert = msg.getParam("fax_divert");
     if (!divert)
 	return;
+    m_detFax = true;
     // if divert is empty or false disable diverting
     if (divert->null() || !divert->toBoolean(true))
 	m_divert.clear();
@@ -312,6 +460,11 @@ ToneDetectorModule::ToneDetectorModule()
 ToneDetectorModule::~ToneDetectorModule()
 {
     Output("Unloading module ToneDetector");
+}
+
+void ToneDetectorModule::statusParams(String& str)
+{
+    str.append("count=",",") << s_count;
 }
 
 void ToneDetectorModule::initialize()

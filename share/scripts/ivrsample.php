337f24a04ddb1e5d47bfc01c29fd1714e5ff00f5
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2008-07-01 19:37:49 +0000

    Added comments to IVR sample.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@2044 acf43c95-373e-0410-b603-e72c3f656dc1

--- share/scripts/ivrsample.php
+++ share/scripts/ivrsample.php
@@ -10,20 +10,30 @@ require_once("libyateivr.php");
 class IVR1 extends IVR
 {
 
+    // do initialization when entering this IVR
     function OnEnter($state)
     {
+	// initialize the operation table on entering the IVR
 	$this->optable = array(
+	    // key 5 - call the 2nd IVR state 'b', allow it to return
 	    ":5" => "call:ivr2:b",
+	    // key 6 - jump to 2nd IVR state 'b', will not return to us
 	    ":6" => "jump:ivr2:b",
+	    // key 8 - leave this IVR, return to parent or exit IVR::Run
 	    ":8" => "leave",
+	    // key 9 - clear play queue, add 2 files and start playing
 	    ":9" => "play:file1.au:file2.slin:clear",
+	    // on call.execute immediately answer the call
 	    ":execute" => "answered",
+	    // when entering this IVR output a message to logs
 	    ":enter" => "output:Entered 1st IVR"
 	);
 	parent::OnEnter($state);
+	// call the 2nd IVR
 	IVR::Call("ivr2");
     }
 
+    // handle keypad tones while in this IVR
     function OnDTMF($tone)
     {
 	$this->Output("Got $tone");
@@ -37,22 +47,29 @@ class IVR1 extends IVR
 
 class TheIVR_2 extends IVR
 {
+    // do initialization when entering this IVR
     function OnEnter($state)
     {
 	parent::OnEnter($state);
+	// this will fail so we remain the current IVR
 	IVR::Call("nosuch");
     }
 
+    // handle keypad tones while in this IVR
     function OnDTMF($tone)
     {
 	switch ($tone) {
 	    case "*":
+		// hang up the IVR channel and possibly the incoming call
 		IVR::Hangup();
 		break;
 	    case "#":
+		// return to calling IVR with value 'Got #'
 		IVR::Leave("Got $tone");
 		break;
 	    case "0":
+	    case "1":
+	    case "2":
 		$this->Output("Got $tone");
 		break;
 	    default:
@@ -62,14 +79,18 @@ class TheIVR_2 extends IVR
     }
 }
 
+// initialize the Yate PHP library with default values, enable debugging
 Yate::Init();
 Yate::Debug(true);
 
+// register the two IVR classes with some names
 IVR::Register("ivr1","IVR1");
 IVR::Register("ivr2","TheIVR_2");
 
+// and start running by entering the 1st IVR
 IVR::Run("ivr1");
 
+// if we reach here the IVR is terminated
 Yate::Output("PHP: bye!");
 
 /* vi: set ts=8 sw=4 sts=4 noet: */

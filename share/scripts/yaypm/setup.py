bcf0ac35959e7a7921a08fc44b66a8a5aa7faa36
maciej <maciej@acf43c95-373e-0410-b603-e72c3f656dc1>
2008-07-02 13:33:08 +0000

    INSTALL renamed, examples __init__.py added.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@2045 acf43c95-373e-0410-b603-e72c3f656dc1

--- share/scripts/yaypm/setup.py
+++ share/scripts/yaypm/setup.py
@@ -26,10 +26,16 @@ This is the distutils installation script for YAYPM.
 '''
 from distutils.core import setup
 setup(
-        name = 'yaypm',
-        version = '0.2',
-        description = 'Yet Another YATE Python Module',
-        author = 'Maciek Kaminski',
-        author_email = 'maciejka@tiger.com.pl',
-        packages = ['yaypm'],
-        )
+    name = 'yaypm',
+    version = '0.2',
+    description = 'Yet Another YATE Python Module',
+    author = 'Maciek Kaminski',
+    author_email = 'maciejka@tiger.com.pl',
+    url = 'http://yate.null.ro/pmwiki/index.php?n=Main.YAYPM',
+    package_dir = {'': '..'},
+    packages = ['yaypm',
+                'yaypm.utils',
+                'yaypm.utils.resources',
+                'yaypm.utils.tester',
+                'yaypm.examples'],
+)

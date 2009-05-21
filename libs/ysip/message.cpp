60173533324a4fb73c288021b258a5fee44a4d97
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2009-05-21 20:57:51 +0000

    Add a Contact header to INVITE even if user part of URI is empty. Properly escape the user component of the URI.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@2645 acf43c95-373e-0410-b603-e72c3f656dc1

--- libs/ysip/message.cpp
+++ libs/ysip/message.cpp
@@ -237,7 +237,7 @@ void SIPMessage::complete(SIPEngine* engine, const char* user, const char* domai
 	if (!hl) {
 	    String tmp = "<sip:";
 	    if (user)
-		tmp << user << "@";
+		tmp << String::uriEscape(user,'@') << "@";
 	    tmp << domain << ">";
 	    hl = new MimeHeaderLine("From",tmp);
 	    header.append(hl);
@@ -289,14 +289,14 @@ void SIPMessage::complete(SIPEngine* engine, const char* user, const char* domai
 	    tmp = uri;
 	    Regexp r(":\\([^:@]*\\)@");
 	    tmp.matches(r);
-	    tmp = tmp.matchString(1);
+	    tmp = tmp.matchString(1).uriUnescape();
 	}
-	if (tmp) {
-	    tmp = "<sip:" + tmp; 
-	    tmp << "@" << getParty()->getLocalAddr() ;
-	    tmp << ":" << getParty()->getLocalPort() << ">";
-	    addHeader("Contact",tmp);
-	}
+	if (tmp)
+	    tmp = tmp.uriEscape('@') + "@";
+	tmp = "<sip:" + tmp; 
+	tmp << getParty()->getLocalAddr() << ":";
+	tmp << getParty()->getLocalPort() << ">";
+	addHeader("Contact",tmp);
     }
 
     if (!getHeader("Allow"))

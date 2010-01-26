28bfb5426b7635f0e1489bd39117fe2516e1f100
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2010-01-26 10:21:30 +0000

    Only needs to include yatengine.h, not yatephone.h
    git-svn-id: http://yate.null.ro/svn/yate/trunk@3032 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/server/lateroute.cpp
+++ modules/server/lateroute.cpp
@@ -22,7 +22,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include <yatephone.h>
+#include <yatengine.h>
 
 using namespace TelEngine;
 namespace { // anonymous

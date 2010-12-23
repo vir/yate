20092c86500428e792116c72f290c15d5f4fae0d
Vasily i. Redkin <vir@ctm.ru>
2010-12-23 15:02:28 +0300

    Fixed unpackaged files listing script

--- debian/checkinstall.sh
+++ debian/checkinstall.sh
@@ -2,22 +2,29 @@
 #
 # (c) vir
 #
-# Last modified: 2009-06-25 16:07:50 +0400
+# Last modified: 2010-12-23 10:33:10 +0300
 #
 
 D=debian/tmp
 LIST=debian/NOT-INSTALLED-LIST
 
-ALLFILES=`find $D -type f -print | cut -d/ -f3- | grep -v '^usr/share/doc' | sed 's/usr\/lib\/yate\/client\//usr\/lib\/yate\//'`
+ALLFILES=`find $D -type f -print | cut -d/ -f3- | grep -v '^usr/share/doc'`
+
 
 echo -n > $LIST
 
 for f in $ALLFILES
 do
-	if [ ! -e debian/*yate*/$f ]
+# some files moved from .../client/, some not - check both
+	f2=`echo $f | sed 's/usr\/lib\/yate\/client\//usr\/lib\/yate\//'`
+	if [ ! -e debian/*yate*/$f -a ! -e debian/*yate*/$f2 ]
 	then
 		echo "$f" >> $LIST
 	fi
 done
+
+echo "*** *** *** NOT PACKAGED FILES: *** *** ***"
+cat $LIST
+echo "~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~ ~~~"
 
 

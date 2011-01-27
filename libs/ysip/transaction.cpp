4ada8776fda841cf776c04e19ddd5a0a35180aaa
Vasily i. Redkin <vir@ctm.ru>
2011-01-27 10:04:58 +0300

    Added SIP auth QOP support (client-side only)

--- libs/ysip/transaction.cpp
+++ libs/ysip/transaction.cpp
@@ -83,7 +83,7 @@ SIPTransaction::SIPTransaction(SIPTransaction& original, SIPMessage* answer)
 	&original,answer,this);
 
     SIPMessage* msg = new SIPMessage(*original.m_firstMessage);
-    MimeAuthLine* auth = answer->buildAuth(*original.m_firstMessage);
+    MimeAuthLine* auth = answer->buildAuth(*original.m_firstMessage, m_engine->m_noncer);
     m_firstMessage->setAutoAuth();
     msg->complete(m_engine);
     msg->addHeader(auth);
@@ -374,7 +374,7 @@ void SIPTransaction::requestAuth(const String& realm, const String& domain, bool
 	MimeHeaderLine* line = new MimeHeaderLine(hdr,tmp,',');
 	if (domain)
 	    line->setParam(" domain",MimeHeaderLine::quote(domain));
-	m_engine->nonceGet(tmp);
+	m_engine->m_noncer.nonceGet(tmp);
 	line->setParam(" nonce",MimeHeaderLine::quote(tmp));
 	line->setParam(" stale",stale ? "TRUE" : "FALSE");
 	line->setParam(" algorithm","MD5");

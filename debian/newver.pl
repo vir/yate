41edd2ea8647e57caa95c6f708940d74e2a73b2e
Debian Package Builder <vir@ctm.ru>
2011-02-01 11:01:15 +0300

    Added debian package version number update script

---
+++ debian/newver.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $verbose = @ARGV;
+
+open CL, "< debian/changelog" or die;
+my $firstline = <CL>;
+$firstline =~/^.*?\([\d\.]+vir(\d{10})\)/i or die;
+my $lastrev = $1;
+close CL;
+print "Found last seen revision $lastrev\n";
+
+my $yatever;
+open CF, "< configure.in" or die;
+while(<CF>) {
+	chomp;
+	if(/^\s*AC_INIT\(Yate, ([\d\.]+)\)/) {
+		$yatever = $1;
+		last;
+	}
+}
+close CF;
+
+my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+my $suff = sprintf("%04d%02d%02d%02d", 1900+$year, 1+$mon, $mday, 1);
+
+++$suff while $suff eq $lastrev;
+
+my $newver="$yatever.vir$suff";
+print "New version: $newver\n";
+#exec qw( debchange -b --preserve --distribution UNRELEASED --newversion ), "$newver";
+
+
+

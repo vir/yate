433e623a7353e64a70853bbd930af7c0bf9ad5d9
marian <marian@acf43c95-373e-0410-b603-e72c3f656dc1>
2011-03-29 12:13:37 +0000

    Fixed bug: avoid clearing rtp media parameters in copy constructor.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@4237 acf43c95-373e-0410-b603-e72c3f656dc1

--- libs/yjabber/yatejingle.h
+++ libs/yjabber/yatejingle.h
@@ -82,9 +82,10 @@ public:
      * Copy constructor
      */
     inline JGRtpMedia(const JGRtpMedia& src)
-	: GenObject(),
-	  m_params(src.m_params)
-	{ set(src.m_id,src.m_name,src.m_clockrate,src.m_synonym,src.m_channels); }
+	: m_params("") {
+	    set(src.m_id,src.m_name,src.m_clockrate,src.m_synonym,src.m_channels);
+	    m_params = src.m_params;
+	}
 
     /**
      * Set the data

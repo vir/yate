e6de5b77a9b6cf265c2ae370aa23f46828b98a72
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2011-05-17 15:36:28 +0000

    Handle more SCTP events that can indicate a closed connection. Return -1 when an event is detected.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@4387 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/server/lksctp.cpp
+++ modules/server/lksctp.cpp
@@ -155,7 +155,7 @@ int LKSocket::recvMsg(void* buf, int length, SocketAddr& addr, int& stream, int&
 	    flags = 2;
 	else
 	    flags = 0;
-	r = 0;
+	r = -1;
     }
     stream = sri.sinfo_stream;
     return r;
@@ -223,8 +223,11 @@ bool LKSocket::getStreams(int& in, int& out)
 bool LKSocket::sctpDown(void* buf)
 {
     union sctp_notification *sn = (union sctp_notification *)buf;
+    DDebug(&plugin,DebugInfo,"Event: 0x%X [%p]",sn->sn_header.sn_type,this);
     switch (sn->sn_header.sn_type) {
 	case SCTP_SHUTDOWN_EVENT:
+	case SCTP_SEND_FAILED:
+	case SCTP_REMOTE_ERROR:
 	    return true;
 	case SCTP_ASSOC_CHANGE:
 	  switch (sn->sn_assoc_change.sac_state) {

b34639ebc7b647245be62ad37da689dcc0e11686
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2011-08-22 15:07:08 +0000

    Error ELAST (500) indicates a Zaptel/DAHDI event during write() too.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@4558 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/server/zapcard.cpp
+++ modules/server/zapcard.cpp
@@ -1457,6 +1457,7 @@ bool ZapDevice::select(unsigned int usec)
 
 int ZapDevice::recv(void* buffer, int len)
 {
+    errno = 0;
     int r = ::read(m_handle,buffer,len);
     if (r >= 0) {
 	m_event = false;
@@ -1477,12 +1478,15 @@ int ZapDevice::recv(void* buffer, int len)
 
 int ZapDevice::send(const void* buffer, int len)
 {
+    errno = 0;
     int w = ::write(m_handle,buffer,len);
     if (w == len) {
 	m_writeError = false;
 	return w;
     }
-    if (!m_writeError) {
+    if (errno == ELAST)
+	m_event = true;
+    else if (!m_writeError) {
 	Debug(m_owner,DebugWarn,
 	    "%sWrite failed on channel %u (sent %d instead of %d). %d: %s [%p]",
 	    m_name.safe(),m_channel,w>=0?w:0,len,errno,::strerror(errno),m_owner);

755917d58c8b0fe1225b363622c684d6ceded105
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2012-02-17 16:34:10 +0000

    Bumped version to 4.0.0 release.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@4922 acf43c95-373e-0410-b603-e72c3f656dc1

--- windows/yateversn.h
+++ windows/yateversn.h
@@ -10,20 +10,20 @@
 #define __YATEVERSN_H
 
 /* Version numbers */
-#define YATE_MAJOR 3
-#define YATE_MINOR 3
-#define YATE_BUILD 3
+#define YATE_MAJOR 4
+#define YATE_MINOR 0
+#define YATE_BUILD 0
 
 /* Version strings */
-#define YATE_MAJOR_S "3"
-#define YATE_MINOR_S "3"
-#define YATE_BUILD_S "3"
-#define YATE_VERSION "3.3.3"
+#define YATE_MAJOR_S "4"
+#define YATE_MINOR_S "0"
+#define YATE_BUILD_S "0"
+#define YATE_VERSION "4.0.0"
 #define YATE_RELEASE "1"
-#define YATE_STATUS  "alpha"
+#define YATE_STATUS  ""
 
 /* Windows version resource - file and string style */
-#define YATE_WINVER_F 3,3,3,1
-#define YATE_WINVER_S "3, 3, 3, 1\0"
+#define YATE_WINVER_F 4,0,0,1
+#define YATE_WINVER_S "4, 0, 0, 1\0"
 
 #endif /* __YATEVERSN_H */

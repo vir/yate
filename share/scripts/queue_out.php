785ce0b6e840d277543bff19c22efc1c11b29399
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2012-04-05 12:31:23 +0000

    Added copyright and licensing information to PHP scripts.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@4997 acf43c95-373e-0410-b603-e72c3f656dc1

--- share/scripts/queue_out.php
+++ share/scripts/queue_out.php
@@ -1,5 +1,27 @@
 #!/usr/bin/php -q
 <?php
+/**
+ * queue_out.php
+ * This file is part of the YATE Project http://YATE.null.ro
+ *
+ * Yet Another Telephony Engine - a fully featured software PBX and IVR
+ * Copyright (C) 2008-2012 Null Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 /*
  * Queue outbound calls (distributed to operators)
  * The queue module will create one instance every time it tries to send a
--- share/scripts/resolver.php
+++ share/scripts/resolver.php
@@ -1,5 +1,27 @@
 #!/usr/bin/php -q
 <?php
+/**
+ * resolver.php
+ * This file is part of the YATE Project http://YATE.null.ro
+ *
+ * Yet Another Telephony Engine - a fully featured software PBX and IVR
+ * Copyright (C) 2009-2012 Null Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
 /* Caching resolver script for the Yate PHP interface
    Needs php pear Net_DNS http://pear.php.net/package/Net_DNS
 

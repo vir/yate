66c0eef8575985883c55bcbc0db1297522b3b521
andrei <andrei@acf43c95-373e-0410-b603-e72c3f656dc1>
2012-06-07 07:35:58 +0000

    Set SS7TcapTransaction mutex name.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@5097 acf43c95-373e-0410-b603-e72c3f656dc1

--- libs/ysig/tcap.cpp
+++ libs/ysig/tcap.cpp
@@ -1125,7 +1125,7 @@ u_int16_t SS7TCAPError::codeFromError(SS7TCAP::TCAPType tcapType, int err)
  */
 SS7TCAPTransaction::SS7TCAPTransaction(SS7TCAP* tcap, SS7TCAP::TCAPUserTransActions type,
 	const String& transactID, NamedList& params, u_int64_t timeout, bool initLocal)
-    : Mutex(true,transactID),
+    : Mutex(true,"TcapTransaction"),
       m_tcap(tcap), m_tcapType(SS7TCAP::UnknownTCAP), m_userName(""), m_localID(transactID), m_type(type),
       m_localSCCPAddr(""), m_remoteSCCPAddr(""), m_basicEnd(true), m_endNow(false), m_timeout(timeout)
 {

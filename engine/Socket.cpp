9eaa1b949886633db23b0e8f8b5f0f1ccf447ed6
oana <oana@acf43c95-373e-0410-b603-e72c3f656dc1>
2012-08-07 15:22:56 +0000

    Try to get socket address length from sa_len field when it is present.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@5219 acf43c95-373e-0410-b603-e72c3f656dc1

--- engine/Socket.cpp
+++ engine/Socket.cpp
@@ -170,6 +170,10 @@ void SocketAddr::assign(const struct sockaddr* addr, socklen_t len)
 {
     if (addr == m_address)
 	return;
+#ifdef HAVE_SOCKADDR_LEN
+    if (addr && !len)
+	len = addr->sa_len;
+#endif
     if (addr && !len) {
 	switch (addr->sa_family) {
 	    case AF_INET:

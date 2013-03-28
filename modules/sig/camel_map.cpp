10dbf4f219bb2c751f616f884a3faee607854494
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2013-03-28 17:00:45 +0000

    Added remaining errors for MAPv3 UMTS V9.4.0 Constified a few error related parameter tables.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@5429 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/sig/camel_map.cpp
+++ modules/sig/camel_map.cpp
@@ -5990,25 +5990,25 @@ static const Operation s_camelOps[] = {
 };
 
 
-const TokenDict s_unknownSubscriberDiagnostic[] = {
+static const TokenDict s_unknownSubscriberDiagnostic[] = {
     {"imsiUnknown",                0},
     {"gprsSubscriptionUnknown",    1},
     {"npdbMismatch",               2},
     {0, 0},
 };
 
-const TokenDict s_roamingNotAllowedCause[] = {
+static const TokenDict s_roamingNotAllowedCause[] = {
     {"plmnRoamingNotAllowed",        0},
     {"operatorDeterminedBarring",    3},
     {0, 0},
 };
 
-const TokenDict s_additionalRoamingNotAllowedCause[] = {
+static const TokenDict s_additionalRoamingNotAllowedCause[] = {
     {"supportedRAT-TypesNotAllowed ",  0},
     {0, 0},
 };
 
-const TokenDict s_absentSubscriberReason[] = {
+static const TokenDict s_absentSubscriberReason[] = {
     {"imsiDetach",      0 },
     {"restrictedArea",  1 },
     {"noPageResponse",  2 },
@@ -6017,7 +6017,7 @@ const TokenDict s_absentSubscriberReason[] = {
     {0, 0},
 };
 
-const TokenDict s_smDeliveryFailureCause[] = {
+static const TokenDict s_smDeliveryFailureCause[] = {
     {"memoryCapacityExceeded",      0 },
     {"equipmentProtocolError",      1 },
     {"equipmentNotSM-Equipped ",    2 },
@@ -6028,7 +6028,7 @@ const TokenDict s_smDeliveryFailureCause[] = {
     {0, 0},
 };
 
-const TokenDict s_networkResource[] = {
+static const TokenDict s_networkResource[] = {
     {"plmn",            0 },
     {"hlr",             1 },
     {"vlr ",            2 },
@@ -6040,7 +6040,7 @@ const TokenDict s_networkResource[] = {
     {0, 0},
 };
 
-const TokenDict s_additionalNetworkResource[] = {
+static const TokenDict s_additionalNetworkResource[] = {
     {"sgsn",   0},
     {"ggsn",   1},
     {"gmlc",   2},
@@ -6052,12 +6052,37 @@ const TokenDict s_additionalNetworkResource[] = {
     {0, 0},
 };
 
-const TokenDict s_failureCauseParam[] = {
+static const TokenDict s_failureCauseParam[] = {
     {"limitReachedOnNumberOfConcurrentLocationRequests", 0},
     {0, 0},
 };
 
-const Parameter s_extensibleSystemFailure[] = {
+static const TokenDict s_unauthorizedLcscDiag[] = {
+    {"noAdditionalInformation",                        0},
+    {"clientNotInMSPrivacyExceptionList",              1},
+    {"callToClientNotSetup",                           2},
+    {"privacyOverrideNotApplicable",                   3},
+    {"disallowedByLocalRegulatoryRequirements",        4},
+    {"unauthorizedPrivacyClass",                       5},
+    {"unauthorizedCallSessionUnrelatedExternalClient", 6},
+    {"unauthorizedCallSessionRelatedExternalClient",   7},
+    {0, 0},
+};
+
+static const TokenDict s_positionMethodFailureDiag[] = {
+    {"congestion",                               0},
+    {"insufficientResources",                    1},
+    {"insufficientMeasurementData",              2},
+    {"inconsistentMeasurementData",              3},
+    {"locationProcedureNotCompleted",            4},
+    {"locationProcedureNotSupportedByTargetMS",  5},
+    {"qoSNotAttainable",                         6},
+    {"positionMethodNotAvailableInNetwork",      7},
+    {"positionMethodNotAvailableInLocationArea", 8},
+    {0, 0},
+};
+
+static const Parameter s_extensibleSystemFailure[] = {
     {"networkResource",                s_enumTag,       true,    TcapXApplication::Enumerated,    s_networkResource},
     {"extensionContainer",             s_sequenceTag,   true,    TcapXApplication::HexString,     0},
     {"additionalNetworkResource",      s_ctxtPrim_0_Tag,true,    TcapXApplication::Enumerated,    s_additionalNetworkResource},
@@ -6065,29 +6090,43 @@ const Parameter s_extensibleSystemFailure[] = {
     {"",                               s_noTag,         false,   TcapXApplication::None,          0},
 };
 
-const Parameter s_systemFailure[] = {
+static const Parameter s_systemFailure[] = {
     {"networkResource",                s_enumTag,       false,    TcapXApplication::Enumerated,    s_networkResource},
     {"extensibleSystemFailure",        s_sequenceTag,   false,    TcapXApplication::Sequence,      s_extensibleSystemFailure},
     {"",                               s_noTag,         false,    TcapXApplication::None,          0},
 };
 
-const TokenDict s_pwRegistrationFailureCause[] = {
+static const TokenDict s_pwRegistrationFailureCause[] = {
     {"undetermined",          0 },
     {"invalidFormat",         1 },
     {"newPasswordsMismatch",  2 },
     {0, 0},
 };
 
-const TokenDict s_callBarringCause[] = {
+static const TokenDict s_callBarringCause[] = {
     {"barringServiceActive",      0 },
     {"operatorBarring",           1 },
     {0, 0},
 };
 
-const Parameter s_extensibleCallBarredParam[] = {
+static const TokenDict s_cugRejectCause[] = {
+    {"incomingCallsBarredWithinCUG",                 0 },
+    {"subscriberNotMemberOfCUG",                     1 },
+    {"requestedBasicServiceViolatesCUG-Constraints", 5 },
+    {"calledPartySS-InteractionViolation",           7 },
+    {0, 0},
+};
+
+static const Parameter s_extensibleCallBarredParam[] = {
     {"callBarringCause",               s_enumTag,       true,    TcapXApplication::Enumerated,    s_callBarringCause},
     {"extensionContainer",             s_sequenceTag,   true,    TcapXApplication::HexString,     0},
     {"unauthorisedMessageOriginator",  s_ctxtPrim_1_Tag,true,    TcapXApplication::Null,          0},
+    {"",                               s_noTag,         false,   TcapXApplication::None,          0},
+};
+
+static const Parameter s_cugRejectErr[] = {
+    {"cug-RejectCause",                s_enumTag,       true,    TcapXApplication::Enumerated,    s_cugRejectCause},
+    {"extensionContainer",             s_sequenceTag,   true,    TcapXApplication::HexString,     0},
     {"",                               s_noTag,         false,   TcapXApplication::None,          0},
 };
 
@@ -6142,6 +6181,12 @@ static const Parameter s_absentSubscriberErr[] = {
     {"",                               s_noTag,           false, TcapXApplication::None,        0},
 };
 
+static const Parameter s_subscriberBusyMtSmsErr[] = {
+    {"extensionContainer",                s_sequenceTag,    true,    TcapXApplication::HexString,     0},
+    {"gprsConnectionSuspended",           s_nullTag,        true,    TcapXApplication::Null,          0},
+    {"",                                  s_noTag,          false,   TcapXApplication::None,          0},
+};
+
 static const Parameter s_smDeliveryFailureErr[] = {
     {"sm-EnumeratedDeliveryFailureCause", s_enumTag,        true,    TcapXApplication::Enumerated,    s_smDeliveryFailureCause},
     {"diagnosticInfo",                    s_hexTag,         true,    TcapXApplication::HexString,     0},
@@ -6159,6 +6204,18 @@ static const Parameter s_busySubscriberErr[] = {
     {"ccbs-Possible",                  s_ctxtPrim_0_Tag,   true,    TcapXApplication::Null,          0},
     {"ccbs-Busy",                      s_ctxtPrim_1_Tag,   true,    TcapXApplication::Null,          0},
     {"",                               s_noTag,            false,   TcapXApplication::None,          0},
+};
+
+static const Parameter s_unauthorizedLcscErr[] = {
+    {"unauthorizedLCSClient-Diagnostic",  s_ctxtPrim_0_Tag, true,    TcapXApplication::Enumerated,    s_unauthorizedLcscDiag},
+    {"extensionContainer",                s_ctxtPrim_1_Tag, true,    TcapXApplication::HexString,     0},
+    {"",                                  s_noTag,          false,   TcapXApplication::None,          0},
+};
+
+static const Parameter s_positionMethodFailureErr[] = {
+    {"positionMethodFailure-Diagnostic",  s_ctxtPrim_0_Tag, true,    TcapXApplication::Enumerated,    s_positionMethodFailureDiag},
+    {"extensionContainer",                s_ctxtPrim_1_Tag, true,    TcapXApplication::HexString,     0},
+    {"",                                  s_noTag,          false,   TcapXApplication::None,          0},
 };
 
 static const Operation s_mapErrors[] = {
@@ -6210,6 +6267,10 @@ static const Operation s_mapErrors[] = {
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },
+    {"cug-Reject", true, 15, -1,
+	s_sequenceTag, s_cugRejectErr,
+	s_noTag, 0
+    },
     {"illegalSS-Operation", true, 16, -1,
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
@@ -6234,8 +6295,36 @@ static const Operation s_mapErrors[] = {
 	s_sequenceTag, s_facilityNotSupportedErr,
 	s_noTag, 0
     },
+    {"ongoingGroupCall", true, 22, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"noHandoverNumberAvailable", true, 25, -1,
+	s_noTag, 0,
+	s_noTag, 0
+    },
+    {"subsequentHandoverFailure", true, 26, -1,
+	s_noTag, 0,
+	s_noTag, 0
+    },
     {"absentSubscriber", true, 27, -1,
 	s_sequenceTag, s_absentSubscriberErr,
+	s_noTag, 0
+    },
+    {"incompatibleTerminal", true, 28, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"shortTermDenial", true, 29, -1,
+	s_sequenceTag, 0,
+	s_noTag, 0
+    },
+    {"longTermDenial", true, 30, -1,
+	s_sequenceTag, 0,
+	s_noTag, 0
+    },
+    {"subscriberBusyForMT-SMS", true, 31, -1,
+	s_sequenceTag, s_subscriberBusyMtSmsErr,
 	s_noTag, 0
     },
     {"sm-DeliveryFailure", true, 32, -1,
@@ -6274,8 +6363,16 @@ static const Operation s_mapErrors[] = {
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },
+    {"targetCellOutsideGroupCallArea", true, 42, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
     {"numberOfPW-AttemptsViolation", true, 43, -1,
 	s_noTag, 0,
+	s_noTag, 0
+    },
+    {"numberChanged", true, 44, -1,
+	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },
     {"busySubscriber", true, 45, -1,
@@ -6283,6 +6380,10 @@ static const Operation s_mapErrors[] = {
 	s_noTag, 0
     },
     {"noSubscriberReply", true, 46, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"forwardingFailed", true, 47, -1,
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },
@@ -6294,11 +6395,43 @@ static const Operation s_mapErrors[] = {
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },
+    {"noGroupCallNumberAvailable", true, 50, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
     {"resourceLimitation", true, 51, -1,
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },
     {"unauthorizedRequestingNetwork", true, 52, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"unauthorizedLCSClient", true, 53, -1,
+	s_sequenceTag, s_unauthorizedLcscErr,
+	s_noTag, 0
+    },
+    {"positionMethodFailure", true, 54, -1,
+	s_sequenceTag, s_positionMethodFailureErr,
+	s_noTag, 0
+    },
+    {"unknownOrUnreachableLCSClient", true, 58, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"mm-EventNotSupported", true, 59, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"atsi-NotAllowed", true, 60, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"atm-NotAllowed", true, 61, -1,
+	s_sequenceTag, s_extensionContainerRes,
+	s_noTag, 0
+    },
+    {"informationNotAvailable", true, 62, -1,
 	s_sequenceTag, s_extensionContainerRes,
 	s_noTag, 0
     },

4d8d9a4a6a73c1e4b95bbbfe4bec43cb69c3a38b
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2013-07-05 13:47:11 +0000

    Fixed comparation in new worker thread alarm level.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@5565 acf43c95-373e-0410-b603-e72c3f656dc1

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -1473,7 +1473,7 @@ int Engine::run()
 	// Create worker thread if we didn't hear about any of them in a while
 	if (s_makeworker && (EnginePrivate::count < s_maxworkers)) {
 	    if (EnginePrivate::count)
-		Alarm("engine","performance",(EnginePrivate::count > 4) ? DebugMild : DebugWarn,
+		Alarm("engine","performance",(EnginePrivate::count < 4) ? DebugMild : DebugWarn,
 		    "Creating new message dispatching thread (%d running)",EnginePrivate::count);
 	    else
 		Debug(DebugInfo,"Creating first message dispatching thread");

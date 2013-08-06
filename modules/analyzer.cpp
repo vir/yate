395d2a3e8a37af1d3ea20a487a6f9188c8446353
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2013-08-06 13:38:10 +0000

    Changed license terms in each source file to reference an external file.
    git-svn-id: http://yate.null.ro/svn/yate/trunk@5609 acf43c95-373e-0410-b603-e72c3f656dc1

--- modules/analyzer.cpp
+++ modules/analyzer.cpp
@@ -5,7 +5,7 @@
  * Test call generator and audio quality analyzer
  *
  * Yet Another Telephony Engine - a fully featured software PBX and IVR
- * Copyright (C) 2004-2006 Null Team
+ * Copyright (C) 2004-2013 Null Team
  *
  * FFT routine taken from Murphy McCauley's FFT DLL based in turn on the
  *  work of Don Cross <dcross@intersrv.com>
@@ -13,19 +13,16 @@
  *  and http://www.constantthought.com/
  * Thank both for a sleek routine that doesn't even fill a floppy ;-)
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This software is distributed under multiple licenses;
+ * see the COPYING file in the main directory for licensing
+ * information for this specific distribution.
+ *
+ * This use of this software may be subject to additional restrictions.
+ * See the LEGAL file in the main directory for details.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #include <yatephone.h>

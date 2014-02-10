82da3cc5802223a57138fcd6d32286c5e674e7d5
paulc <paulc@acf43c95-373e-0410-b603-e72c3f656dc1>
2014-02-10 11:12:01 +0000

    Bumped version to 5.1.0
    git-svn-id: http://yate.null.ro/svn/yate/trunk@5767 acf43c95-373e-0410-b603-e72c3f656dc1

--- windows/yateversn.h
+++ windows/yateversn.h
@@ -1,9 +1,23 @@
-/*
+/**
  * yateversn.h
  * This file is part of the YATE Project http://YATE.null.ro
  *
  * This is a generated file. You should never need to modify it.
  * Take a look at the source file yateversn.h.in instead.
+ *
+ * Yet Another Telephony Engine - a fully featured software PBX and IVR
+ * Copyright (C) 2004-2014 Null Team
+ *
+ * This software is distributed under multiple licenses;
+ * see the COPYING file in the main directory for licensing
+ * information for this specific distribution.
+ *
+ * This use of this software may be subject to additional restrictions.
+ * See the LEGAL file in the main directory for details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #ifndef __YATEVERSN_H
@@ -11,20 +25,20 @@
 
 /* Version numbers */
 #define YATE_MAJOR 5
-#define YATE_MINOR 0
-#define YATE_BUILD 1
+#define YATE_MINOR 1
+#define YATE_BUILD 0
 
 /* Version strings */
 #define YATE_MAJOR_S "5"
-#define YATE_MINOR_S "0"
-#define YATE_BUILD_S "1"
-#define YATE_VERSION "5.0.1"
+#define YATE_MINOR_S "1"
+#define YATE_BUILD_S "0"
+#define YATE_VERSION "5.1.0"
 #define YATE_RELEASE "1"
-#define YATE_STATUS  "alpha"
-#define YATE_REVISION "5676"
+#define YATE_STATUS  ""
+#define YATE_REVISION "5766"
 
 /* Windows version resource - file and string style */
-#define YATE_WINVER_F 5,0,1,1
-#define YATE_WINVER_S "5, 0, 1, 1\0"
+#define YATE_WINVER_F 5,1,0,1
+#define YATE_WINVER_S "5, 1, 0, 1\0"
 
 #endif /* __YATEVERSN_H */
